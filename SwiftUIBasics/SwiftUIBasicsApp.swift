93f52653870aaeb486293e6be959ab0a73fcdc6e
Skander Bahri <s.bahri@workflow-dev.fr>
2020-11-30 18:03:22 +0100

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Skander Bahri on 30/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
