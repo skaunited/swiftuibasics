3d777b1d83f1b5e670a0a5e17cce15a87db91db8
Skander Bahri <s.bahri@workflow-dev.fr>
2020-11-30 18:44:58 +0100

    LazyStack Part2

--- SwiftUIBasics/Layout Views/LazyVStack1.swift
+++ SwiftUIBasics/Layout Views/LazyVStack1.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct LazyVStack1: View {
     var body: some View {
         ScrollView {
-            LazyVStack(alignment: .leading){
-                Section {
-                    ForEach(0 ..< 50) { item in
-                        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            LazyVStack(alignment: .leading,spacing : 40, pinnedViews: [.sectionHeaders, .sectionFooters]){
+                ForEach(0 ..< 5) { item in
+                    Section(header: HeaderView(sectionNumber: item), footer: FooterView(footerNumber: item)) {
+                        ForEach(0 ..< 50) { item in
+                            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                        }
                     }
                 }
             }
@@ -25,5 +27,24 @@ struct LazyVStack1: View {
 struct LazyVStack_Previews: PreviewProvider {
     static var previews: some View {
         LazyVStack1()
+    }
+}
+
+struct HeaderView: View {
+    var sectionNumber = 0
+    var body: some View {
+        Text("Header \(sectionNumber)")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct FooterView: View {
+    var footerNumber = 0
+    var body: some View {
+        Text("Footer \(footerNumber)")
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.5))
     }
 }
